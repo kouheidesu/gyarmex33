94197150e45dfbbd54755c6e75bc5f1d4cdd004c
kouheidesu <gyarmex@gmail.com>
2021-08-19 11:04:13 +0900

    first-commit

---
+++ twitterpj/resources/views/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css/index.css">
+	<link rel="stylesheet" href="style.css/reset.css">
+
+	<title>Document</title>
+</head>
+
+<body>
+	<header>
+		<h1 class="headline">
+			<a>SHARE</a>
+		</h1>
+		<ul class="nav-list">
+			<li class="nav-list-item">新規登録</li>
+			<li class="nav-list-item">ログイン</li>
+		</ul>
+	</header>
+</body>
+
+</html>
